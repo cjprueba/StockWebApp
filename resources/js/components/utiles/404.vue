dc05fa3c1e89386821dfdc77cad864472dce03b5
Cristian HP <cjunnior2010@hotmail.com>
2019-11-19 09:40:30 -0300

    Modificación de la pagina 404

--- resources/js/components/utiles/404.vue
+++ resources/js/components/utiles/404.vue
@@ -8,7 +8,7 @@
           <div class="text-center mt-3">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Página no encontrada</p>
-            <p class="text-gray-500 mb-0">Parece que no tienes permisos por aqui...</p>
+            <p class="text-gray-500 mb-0">Parece que no tienes permisos por aqui por favor acceda en otro lugar...</p>
             <a href="/home">&larr; Vuelve al Inicio</a>
           </div>
 
