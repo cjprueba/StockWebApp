40c9a052c07826d7af8ed91e7ab01987c4e3d10c
C.J. Junnior castro vazquez <cjunnior2010@hotmail.com>
2020-10-27 15:20:58 -0300

    Modificacion para traer pull origin develop

--- resources/js/common.js
+++ resources/js/common.js
@@ -5267,6 +5267,39 @@ function generarPdfNotaCreditoCommon(id){
 
 }
 
+function notaCreditoClienteCreditoCommon(datos){
+
+			// ------------------------------------------------------------------------
+
+			// GUARDAR LA NOTA DE CREDITO
+			
+			return axios.post('/venta/nota/credito/asingar/credito/cliente', {'datos': datos}).then(function (response) {
+					return response.data;
+			});
+
+			// ------------------------------------------------------------------------
+
+}
+
+function obtenerAutorizacion(data){
+	
+			// ------------------------------------------------------------------------
+
+			// INICIAR VARIABLES
+
+			let me = this;
+
+			// ------------------------------------------------------------------------
+
+			// OBTENER PERMISO
+
+			return axios.post('/obtener/autorizacion', {'codigo':data}).then(function (response) {
+					return response.data;
+			});
+
+			// ------------------------------------------------------------------------
+}
+
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
@@ -5493,5 +5526,7 @@ export {
 		generarCuerpoNotaCreditoCommon,
 		guardarNotaCreditoCommon,
 		generarPdfNotaCreditoCommon,
-		aplicarCuponCommon
+		aplicarCuponCommon,
+		notaCreditoClienteCreditoCommon,
+		obtenerAutorizacion
 		};
