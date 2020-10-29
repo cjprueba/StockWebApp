299c71938546323f9df8c55bfa3b41c77db3f342
alezcano <aledanielnexus@gmail.com>
2020-10-29 11:28:25 -0300

    web confirmar aviso

--- routes/web.php
+++ routes/web.php
@@ -608,7 +608,7 @@ Route::get('salida/producto/detalle', 'SalidaProductoController@salidaProductoDe
 
 /* -------------------------------------------------------------------------- */
 Route::get('aviso/obtener', 'EspecificacionController@obtenerAviso');
-Route::post('aviso/confirmar', 'EspecificacionController@obtenerAviso');
+Route::post('aviso/confirmar', 'EspecificacionController@aceptarTerminos');
 
 
 /* -------------------------------------------------------------------------- */
