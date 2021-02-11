e3a17157564dd05ef0e5c114ed4ea0004df517b0
alezcano <aledanielnexus@gmail.com>
2021-02-11 09:37:11 -0300

    reporte vencidos funcional

--- routes/web.php
+++ routes/web.php
@@ -485,7 +485,7 @@ Route::post('exportCompra', 'CompraController@Descargar');
 Route::post('export/Stock', 'StockController@descargar');
 Route::post('export/Stock/Image', 'StockController@descargarImageStock');
 Route::post('export_venta_periodo', 'StockController@descargarPeriodoStock');
-Route::post('export_productos_vencimiento', 'StockController@descagrarVencimientoProducto');
+Route::post('export_producto_vencimiento', 'StockController@descagrarVencimientoProducto');
 
 /* -------------------------------------------------------------------------- */
 
