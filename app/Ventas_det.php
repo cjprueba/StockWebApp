8ac299550d17e53fd91d0f697175448a1b879af6
unknown <cjunnior2010@hotmail.com>
2021-09-24 15:08:29 -0400

    Error anular venta Ventas_det.php

--- app/Ventas_det.php
+++ app/Ventas_det.php
@@ -10,6 +10,7 @@ class Ventas_det extends Model
 
     protected $connection = 'retail';
 	protected $table = 'ventasdet';
+    public $timestamps = false;
 
 	/*  --------------------------------------------------------------------------------- */
 	
